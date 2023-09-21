6230e337daf94a69251feb81c083c59ba5e36325
Eric Stokes <eestokes@pm.me>
2023-09-20 23:41:54 -0400

    event translation

--- src/dcs.rs
+++ src/dcs.rs
@@ -3,15 +3,14 @@ use netidx_derive::Pack;
 use serde_derive::{Deserialize, Serialize};
 use std::ops::{Deref, DerefMut};
 
-fn as_tbl<'lua>(to: &'static str, value: Value<'lua>) -> LuaResult<mlua::Table<'lua>> {
-    match value {
-        Value::Table(t) => Ok(t),
-        _ => Err(LuaError::FromLuaConversionError {
+fn as_tbl<'a: 'lua, 'lua>(to: &'static str, value: &'a Value<'lua>) -> LuaResult<&'a mlua::Table<'lua>> {
+    value
+        .as_table()
+        .ok_or_else(|| LuaError::FromLuaConversionError {
             from: "value",
             to,
             message: None,
-        }),
-    }
+        })
 }
 
 #[derive(Debug, Clone, PartialEq, PartialOrd, Pack, Serialize, Deserialize)]
@@ -65,7 +64,7 @@ pub struct ObjectId(u32);
 
 impl<'lua> FromLua<'lua> for ObjectId {
     fn from_lua(value: Value<'lua>, _: &'lua Lua) -> LuaResult<Self> {
-        let tbl = as_tbl("ObjectId", value)?;
+        let tbl = as_tbl("ObjectId", &value)?;
         Ok(Self(tbl.raw_get("id_")?))
     }
 }
@@ -135,7 +134,7 @@ pub struct Shot {
 
 impl<'lua> FromLua<'lua> for Shot {
     fn from_lua(value: Value<'lua>, _: &'lua Lua) -> LuaResult<Self> {
-        let tbl = as_tbl("Shot", value)?;
+        let tbl = as_tbl("Shot", &value)?;
         Ok(Self {
             time: tbl.raw_get("time")?,
             initiator: tbl.raw_get("initiator")?,
@@ -155,7 +154,7 @@ pub struct WeaponUse {
 
 impl<'lua> FromLua<'lua> for WeaponUse {
     fn from_lua(value: Value<'lua>, _: &'lua Lua) -> LuaResult<Self> {
-        let tbl = as_tbl("WeaponUse", value)?;
+        let tbl = as_tbl("WeaponUse", &value)?;
         Ok(Self {
             time: tbl.raw_get("time")?,
             initiator: tbl.raw_get("initiator")?,
@@ -173,7 +172,7 @@ pub struct UnitEvent {
 
 impl<'lua> FromLua<'lua> for UnitEvent {
     fn from_lua(value: Value<'lua>, _: &'lua Lua) -> LuaResult<Self> {
-        let tbl = as_tbl("UnitEvent", value)?;
+        let tbl = as_tbl("UnitEvent", &value)?;
         Ok(Self {
             time: tbl.raw_get("time")?,
             initiator: tbl.raw_get("initiator")?,
@@ -189,10 +188,15 @@ pub struct AtPlace {
     pub subplace: u32,
 }
 
-impl<'lua> FromLua<'lua> for AtPlace{
-    fn from_lua(value: Value<'lua>, lua: &'lua Lua) -> LuaResult<Self> {
-        let tbl = as_tbl("AtPlace", value)?;
-        
+impl<'lua> FromLua<'lua> for AtPlace {
+    fn from_lua(value: Value<'lua>, _: &'lua Lua) -> LuaResult<Self> {
+        let tbl = as_tbl("AtPlace", &value)?;
+        Ok(Self {
+            time: tbl.raw_get("time")?,
+            initiator: tbl.raw_get("initiator")?,
+            place: tbl.raw_get("place")?,
+            subplace: tbl.raw_get("subplace")?,
+        })
     }
 }
 
@@ -221,6 +225,7 @@ pub enum Event {
     EngineShutdown(AtPlace),
     PlayerEnterUnit(UnitEvent),
     PlayerLeaveUnit,
+    PlayerComment,
     ShootingStart(WeaponUse),
     ShootingEnd(Shot),
     MarkAdded,
@@ -250,4 +255,68 @@ pub enum Event {
     UnitTaskTimeout,
     UnitTaskStage,
     Max,
+    Unknown(u32)
+}
+
+impl<'lua> FromLua<'lua> for Event {
+    fn from_lua(value: Value<'lua>, lua: &'lua Lua) -> LuaResult<Self> {
+        let tbl = as_tbl("Event", &value)?;
+        let id: u32 = tbl.raw_get("id")?;
+        let ev = match id {
+            0 => Event::Invalid,
+            1 => Event::Shot(Shot::from_lua(value, lua)?),
+            2 => Event::Hit(WeaponUse::from_lua(value, lua)?),
+            3 => Event::Takeoff(AtPlace::from_lua(value, lua)?),
+            4 => Event::Land(AtPlace::from_lua(value, lua)?),
+            5 => Event::Crash,
+            6 => Event::Ejection,
+            7 => Event::Refueling,
+            8 => Event::Dead(UnitEvent::from_lua(value, lua)?),
+            9 => Event::PilotDead,
+            10 => Event::BaseCaptured,
+            11 => Event::MissionStart,
+            12 => Event::MissionEnd,
+            13 => Event::TookControl,
+            14 => Event::RefuelingStop,
+            15 => Event::Birth(AtPlace::from_lua(value, lua)?),
+            16 => Event::HumanFailure,
+            17 => Event::DetailedFailure,
+            18 => Event::EngineStartup(AtPlace::from_lua(value, lua)?),
+            19 => Event::EngineShutdown(AtPlace::from_lua(value, lua)?),
+            20 => Event::PlayerEnterUnit(UnitEvent::from_lua(value, lua)?),
+            21 => Event::PlayerLeaveUnit,
+            22 => Event::PlayerComment,
+            23 => Event::ShootingStart(WeaponUse::from_lua(value, lua)?),
+            24 => Event::ShootingEnd(Shot::from_lua(value, lua)?),
+            25 => Event::MarkAdded,
+            26 => Event::MarkChange,
+            27 => Event::MarkRemoved,
+            28 => Event::Kill(WeaponUse::from_lua(value, lua)?),
+            29 => Event::Score(UnitEvent::from_lua(value, lua)?),
+            30 => Event::UnitLost(UnitEvent::from_lua(value, lua)?),
+            31 => Event::LandingAfterEjection,
+            32 => Event::ParatrooperLanding,
+            33 => Event::DiscardChairAfterEjection,
+            34 => Event::WeaponAdd,
+            35 => Event::TriggerZone,
+            36 => Event::LandingQualityMark,
+            37 => Event::Bda,
+            38 => Event::AiAbortMission,
+            39 => Event::DayNight,
+            40 => Event::FlightTime,
+            41 => Event::PlayerSelfKillPilot,
+            42 => Event::PlayerCaptureAirfield,
+            43 => Event::EmergencyLanding,
+            44 => Event::UnitCreateTask,
+            45 => Event::UnitDeleteTask,
+            46 => Event::SimulationStart,
+            47 => Event::WeaponRearm,
+            48 => Event::WeaponDrop,
+            49 => Event::UnitTaskTimeout,
+            50 => Event::UnitTaskStage,
+            51 => Event::Max,
+            u => Event::Unknown(u),
+        };
+        Ok(ev)
+    }
 }

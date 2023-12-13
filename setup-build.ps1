c699477f56e089f9de6497ef6af9ac9116e8405b
Eric Stokes <eestokes@pm.me>
2023-12-13 14:20:58 -0500

    add a script to build a .lib from a .dll, add the current .lib for the latest openbeta, add some docs to setup-build.ps1

--- setup-build.ps1
+++ setup-build.ps1
@@ -1,18 +1,23 @@
-# set this to your dcs bin-mt directory
-$env:LUA_LIB="C:\\Program Files\\Eagle Dynamics\\DCS World\\bin-mt"
+# Why?
+# In order to safely interface with DCS we need to link to the same version of the lua runtime
+# that DCS is using. If we link to a different version, very bad things like structures not being compatible,
+# globals being in the wrong place, or in two places, the GC of one version running on the heap of the other
+# version, etc might happen. There's a huge list, and you just don't want to go there.
+#
+# Setting the environment variables below tells the mlua build scripts that it should produce a dll linked
+# dynamically to the lua runtime, meaning it will not include one of it's own, it will just assume that
+# when our dll is loaded, lua is already linked, which DCS will ensure is true, because it loads lua.dll
+# very early on.
+#
+# In order to link dynamically to lua, we need a .lib file that describes all the functions exported by the dll,
+# so that the linker can check that the dynamic linkage will work at run time. That is why there is a lua.lib
+# checked in at the root of the repo.
+#
+# When a new version of DCS is released, it *could* use a different version of lua (although ED has not upgraded
+# their lua in aaaaaaages). As such every time DCS is updated we should copy lua.dll from the bin-mt folder of DCS
+# to this directory and run dll2lib.bat on it to generate a new lua.lib file. 
+# (you must have the windows sdk installed and the tools in your path for that to work).
+
+$env:LUA_LIB=pwd
 $env:LUA_LINK="dylib"
-
-# we have an open issue with mlua team to figure out why this doesn't work as expected
-# at the moment setting it causes us to try to link to the static lua lib, 
-# which doesn't exist. E.D. calls their lua library lua.dll, even though it's
-# lua 5.1. The canonical name for that is lua51.dll. We are supposed to be be
-# able to set the LUA_LIB_NAME environment variable to anything we want, however
-# if we do, the build will currently fail to link.
-
-# $env:LUA_LIB_NAME="lua"
-
-# we have to make the below symlink to work around LUA_LIB_NAME not working
-# you will have to run this as administrator in your dcs bin-mt folder once
-# before using bflib.dll
-
-# new-item -itemtype SymbolicLink -Path "lua51.dll" -Value 'lua.dll'
+$env:LUA_LIB_NAME="lua"

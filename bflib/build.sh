cd11354cc79ea467f6e77e8df9af48cbf9fc1f31
Eric Stokes <eestokes@pm.me>
2023-12-29 19:56:04 -0500

    latest mission and dll

--- bflib/build.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-export LUA_LIB="/c/Program Files/Eagle Dynamics/DCS World/bin"
-export LUA_LIB_NAME="lua"
-export LUA_LINK=dylib
-cargo build $@
---
+++ setup-build.sh
@@ -0,0 +1,3 @@
+export LUA_LIB=$(pwd)
+export LUA_LIB_NAME="lua"
+export LUA_LINK=dylib

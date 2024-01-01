8ffaa273912d475af5db2bd63ff1e8202bd7dada
Eric Stokes <eestokes@pm.me>
2023-12-31 23:14:55 -0500

    progress

--- bflib/src/jtac.rs
+++ bflib/src/jtac.rs
@@ -1,5 +1,6 @@
-use crate::{cfg::UnitTag, db::UnitId};
-use dcso3::{coalition::Side, Vector2};
+use crate::{cfg::UnitTag, db::{UnitId, Db}};
+use anyhow::Result;
+use dcso3::{coalition::Side, Vector2, spot::ClassSpot, object::DcsOid, MizLua};
 use enumflags2::BitFlags;
 use fxhash::FxHashMap;
 
@@ -8,13 +9,20 @@ struct Jtac {
     contacts: Vec<(BitFlags<UnitTag>, UnitId)>,
     filter: BitFlags<UnitTag>,
     priority: Vec<UnitTag>,
-    target: Option<UnitId>,
+    target: Option<(DcsOid<ClassSpot>, UnitId)>,
     autolase: bool,
     smoketarget: bool,
     code: u16,
     pos: Vector2,
 }
 
+#[derive(Debug, Clone, Default)]
 struct Jtacs {
     jtacs: FxHashMap<Side, FxHashMap<UnitId, Jtac>>,
+}
+
+impl Jtacs {
+    pub fn update_contacts(&mut self, lua: MizLua, db: &Db) -> Result<()> {
+        unimplemented!()
+    }
 }

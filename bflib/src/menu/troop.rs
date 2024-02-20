69325875e8e6dfa9af0afdb76d00df773a838cc9
Eric Stokes <eestokes@pm.me>
2024-02-20 15:56:34 -0500

    license new files

--- bflib/src/menu/troop.rs
+++ bflib/src/menu/troop.rs
@@ -1,3 +1,19 @@
+/*
+Copyright 2024 Eric Stokes.
+
+This file is part of bflib.
+
+bflib is free software: you can redistribute it and/or modify it under
+the terms of the GNU Affero Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your
+option) any later version.
+
+bflib is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero Public License
+for more details.
+*/
+
 use super::{cargo, player_name, slot_for_group, ArgTuple};
 use crate::{
     cfg::{Cfg, LimitEnforceTyp},

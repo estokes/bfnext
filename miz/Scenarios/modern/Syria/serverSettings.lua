98c0ceeedc92a5c8f6fa76cdfa193ba79b8b3364
Eric Stokes <eestokes@pm.me>
2024-02-23 11:08:16 -0500

    add server settings and autoexec

---
+++ miz/Scenarios/modern/Syria/serverSettings.lua
@@ -0,0 +1,48 @@
+cfg = 
+{
+    ["description"] = "It's a cluckin good time!",
+    ["require_pure_textures"] = true,
+    ["listStartIndex"] = 1,
+    ["advanced"] = 
+    {
+        ["allow_change_tailno"] = false,
+        ["disable_events"] = false,
+        ["allow_ownship_export"] = true,
+        ["allow_object_export"] = false,
+        ["pause_on_load"] = true,
+        ["allow_sensor_export"] = false,
+        ["event_Takeoff"] = false,
+        ["pause_without_clients"] = false,
+        ["client_outbound_limit"] = 0,
+        ["client_inbound_limit"] = 0,
+        ["server_can_screenshot"] = true,
+        ["allow_players_pool"] = true,
+        ["voice_chat_server"] = false,
+        ["allow_change_skin"] = true,
+        ["event_Connect"] = true,
+        ["event_Ejecting"] = false,
+        ["event_Kill"] = false,
+        ["event_Crash"] = false,
+        ["event_Role"] = false,
+        ["resume_mode"] = 1,
+        ["maxPing"] = 0,
+        ["allow_trial_only_clients"] = false,
+        ["allow_dynamic_radio"] = false,
+    }, -- end of ["advanced"]
+    ["port"] = 10308,
+    ["mode"] = 0,
+    ["bind_address"] = "135.125.189.202",
+    ["isPublic"] = true,
+    ["missionList"] = 
+    {
+        [1] = "C:\\users\\dcs\\Saved Games\\syria_modern\\Missions\\Operation Fowl Intent - Modern.miz",
+    }, -- end of ["missionList"]
+    ["password"] = "",
+    ["listLoop"] = false,
+    ["name"] = "The Coop - Modern",
+    ["require_pure_scripts"] = true,
+    ["listShuffle"] = false,
+    ["require_pure_clients"] = true,
+    ["require_pure_models"] = true,
+    ["maxPlayers"] = "64",
+} -- end of cfg

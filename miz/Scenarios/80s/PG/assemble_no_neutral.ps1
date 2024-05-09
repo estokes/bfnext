34c7a9e9ea01e7b53d9198fcc04ce324058427fd
Eric Stokes <eestokes@pm.me>
2024-05-09 15:23:33 -0400

    change some final settings for PG

--- miz/Scenarios/80s/PG/assemble_no_neutral.ps1
+++
@@ -1,3 +0,0 @@
-$env:RUST_LOG="info"
-Set-Location $PSScriptRoot
-../../../../bftools/bftools.exe miz --output ./PG_no_neutral.miz --base ./basenoneutral.miz --weapon ../weapon.miz --warehouse ../warehouse.miz --options ../options.miz

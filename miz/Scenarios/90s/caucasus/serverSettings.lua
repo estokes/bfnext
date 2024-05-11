d5190df231fc1adc3e6d50e728654b56c661d8d2
Eric Stokes <eestokes@pm.me>
2024-05-11 12:47:05 -0400

    fix miz file path

--- miz/Scenarios/90s/caucasus/serverSettings.lua
+++ miz/Scenarios/90s/caucasus/serverSettings.lua
@@ -35,7 +35,7 @@ cfg =
     ["isPublic"] = true,
     ["missionList"] = 
     {
-        [1] = "C:\\users\\dcs\\Saved Games\\caucasus_80s\\Missions\\Operation Fowl Intent - 90s.miz",
+        [1] = "C:\\users\\dcs\\Saved Games\\caucasus_90s\\Missions\\Operation Fowl Intent - 90s.miz",
     }, -- end of ["missionList"]
     ["password"] = "",
     ["listLoop"] = false,

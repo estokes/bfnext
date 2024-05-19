af155e05ed03362705e479f9d35f68522f4ad7a3
Eric Stokes <eestokes@pm.me>
2024-05-19 18:59:44 -0400

    fix 90s

--- miz/Scenarios/90s/caucasus/assemble.ps1
+++ miz/Scenarios/90s/caucasus/assemble.ps1
@@ -1,3 +1,3 @@
 $env:RUST_LOG="info"
 Set-Location $PSScriptRoot
-../../../../bftools/bftools.exe miz --output ./Caucasus90s.miz --base ./base.miz --weapon ../weapon.miz --warehouse ../warehouse.miz --options ../options.miz
+../../../../bftools/bftools.exe miz --output ./Caucasus.miz --base ./base.miz --weapon ../weapon.miz --warehouse ../warehouse.miz --options ../options.miz

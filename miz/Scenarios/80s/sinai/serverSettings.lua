2a01aaaa8ae4ebcdebc5e8022e55968fb6dad941
Eric Stokes <eestokes@pm.me>
2024-06-06 18:29:53 -0400

    server port

--- miz/Scenarios/80s/sinai/serverSettings.lua
+++ miz/Scenarios/80s/sinai/serverSettings.lua
@@ -29,7 +29,7 @@ cfg =
         ["allow_trial_only_clients"] = false,
         ["allow_dynamic_radio"] = false,
     }, -- end of ["advanced"]
-    ["port"] = 10309,
+    ["port"] = 10312,
     ["mode"] = 0,
     ["bind_address"] = "135.125.189.202",
     ["isPublic"] = true,

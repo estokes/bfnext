c989a12d44568e1762717e63dae37cdedbd6bff0
Eric Stokes <eestokes@pm.me>
2024-07-06 13:15:41 -0400

    progress

--- miz/Scenarios/80s/Syria/assemble.sh
+++ miz/Scenarios/80s/Syria/assemble.sh
@@ -2,4 +2,4 @@
 
 export RUST_LOG="info"
 cd $(dirname ${BASH_SOURCE[0]})
-cargo run --manifest-path=../../../../bftools/Cargo.toml miz --output ./Syria.miz --base ./base.miz --weapon ../weapon.miz --warehouse ../warehouse.miz --options ../options.miz
+cargo run --manifest-path=../../../../bftools/Cargo.toml miz --output ./Syria80s.miz --base ./base.miz --weapon ../weapon.miz --warehouse ../warehouse.miz --options ../options.miz

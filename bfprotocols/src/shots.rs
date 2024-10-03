8c37824ca3bd0ff43d179243513994e5a60f958c
Eric Stokes <eestokes@pm.me>
2024-10-03 09:44:00 -0400

    rework shots

--- bfprotocols/src/shots.rs
+++ bfprotocols/src/shots.rs
@@ -1,7 +1,12 @@
 use crate::db::group::{GroupId, UnitId};
 use chrono::prelude::*;
 use dcso3::{
-    coalition::Side, net::Ucid, object::DcsOid, unit::ClassUnit, weapon::ClassWeapon, String,
+    coalition::Side,
+    net::{SlotId, Ucid},
+    object::DcsOid,
+    unit::ClassUnit,
+    weapon::ClassWeapon,
+    String,
 };
 use serde_derive::{Deserialize, Serialize};
 
@@ -9,20 +14,22 @@ use serde_derive::{Deserialize, Serialize};
 pub enum Who {
     AI {
         unit: DcsOid<ClassUnit>,
+        side: Side,
         gid: GroupId,
         uid: UnitId,
-        ucid: Option<Ucid>
+        ucid: Option<Ucid>, // deployed
     },
     Player {
         unit: DcsOid<ClassUnit>,
-        ucid: Ucid
+        side: Side,
+        ucid: Ucid,
+        slot: SlotId,
     },
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Dead {
     pub victim: Who,
-    pub victim_side: Side,
     pub time: DateTime<Utc>,
     pub shots: Vec<Shot>,
 }

83aa40e5ce252172a26c24092b46331ea18100a6
Brian <b.d.sandford@gmail.com>
2025-09-05 00:59:34 -0700

    krgnfrdklgjmrdekilog

--- bflib/src/jtac.rs
+++ bflib/src/jtac.rs
@@ -55,7 +55,7 @@ use mlua::{FromLua, IntoLua, Lua, Table, Value, prelude::LuaResult};
 use rand::{Rng, thread_rng};
 use serde::{Deserialize, Serialize};
 use smallvec::{SmallVec, smallvec};
-use std::{collections::hash_map::Entry, fmt, str::FromStr};
+use std::{collections::hash_map::Entry, f32::consts::E, fmt, str::FromStr};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum JtId {
@@ -779,11 +779,8 @@ impl Jtac {
 
                 let mut bombing_task_vec: Vec<MissionPoint> = vec![];
                 let mut fire_task_vec: Vec<Task> = vec![];
-<<<<<<< HEAD
 
                 info!("allocated ammo: {} {}", allocated_ammo, per_target);
-=======
->>>>>>> 0005ae2f (woops)
 
                 for (_, target) in &self.contacts {
                     if allocated_ammo >= per_target {
@@ -1123,7 +1120,11 @@ impl Jtacs {
         lua: MizLua,
         jtid: &JtId,
         shooter: &GroupId,
+<<<<<<< HEAD
         n: Vec<u8>,
+=======
+        n: (u8, u8),
+>>>>>>> 7e1419d2 (krgnfrdklgjmrdekilog)
     ) -> Result<()> {
         let jtac = self
             .jtacs

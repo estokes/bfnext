47d65d2d69630afbc9e22a68a0de99375c66d1fb
Robo76cz <robo76@seznam.cz>
2026-02-07 11:17:30 +0100

    FogOfWar

--- miz/Scenarios/80s/caucasus/assemble.ps1
+++ miz/Scenarios/80s/caucasus/assemble.ps1
@@ -1,3 +1,6 @@
 $env:RUST_LOG="info"
 Set-Location $PSScriptRoot
 ../../../../bftools/bftools.exe miz --output ./Caucasus.miz --base ./base.miz --weapon ./weapon.miz --warehouse ./warehouse.miz --options ./options.miz
+
+# This line prevents the window from closing automatically
+Read-Host -Prompt "Press Enter to exit"
